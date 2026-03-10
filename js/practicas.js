fe7af67eb3c11d821cb77e8abe6d6ee44ced7b93
Eduardo Hernández <eduardo007hernandez19@gmail.com>
2026-03-09 18:02:01 -0700

    3

---
+++ js/practicas.js
@@ -0,0 +1,21 @@
+fetch("data/practicas.json")
+  .then((respuesta) => respuesta.json())
+  .then((practicas) => {
+    const contenedor = document.getElementById("contenedor-practicas");
+
+    practicas.forEach((practica) => {
+      const card = document.createElement("div");
+
+      card.className = "card";
+
+      card.innerHTML = `
+<h3>${practica.nombre}</h3>
+<p>${practica.descripcion}</p>
+<a href="practicas/${practica.archivo}" target="_blank">
+Ver practicas
+</a>
+`;
+
+      contenedor.appendChild(card);
+    });
+  });
